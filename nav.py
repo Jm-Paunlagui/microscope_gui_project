9037d4925d8b084f0067c18fcc83a0718eab6ffe
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2023-02-16 16:44:54 +0800

    hmmmm

--- nav.py
+++ nav.py
@@ -1,3 +1,4 @@
+import customtkinter
 import customtkinter as ctk
 
 
@@ -7,20 +8,16 @@ class NavigationBar(ctk.CTkFrame):
 
         # Set the app title
         self.app_title = ctk.CTkLabel(self, text="My App", font=("Helvetica", 20))
-        self.app_title.pack(side="left", padx=10)
-
-        # Add an empty space between the app title and the hamburger button
-        spacer = ctk.CTkFrame(self, width=10)
-        spacer.pack(side="left")
+        self.app_title.pack(side="top", padx=10)
 
         # Create the hamburger menu
         self.menu_button = ctk.CTkButton(self, text="☰", font=("Helvetica", 20),
                                          command=self.toggle_menu)
-        self.menu_button.pack(side="left")
+        self.menu_button.pack(side="top")
 
         # Create the menu
-        self.menu_frame = ctk.CTkFrame(self, bg_color="#EEE")
-        self.menu_frame.place(x=-200, y=self.app_title.winfo_height())  # Place menu offscreen
+        self.menu_frame = ctk.CTkFrame(self, bg_color="#EEE", height=800)
+        self.menu_frame.place(x=-900, y=self.app_title.winfo_height())  # Place menu offscreen
         self.menu_visible = False
 
         # Add some menu items
@@ -34,13 +31,18 @@ class NavigationBar(ctk.CTkFrame):
                                        font=("Helvetica", 16), bg_color="#EEE")
         self.menu_item3.pack(pady=10, padx=20)
 
+        # Main Content Frame
+        self.content_frame = ctk.CTkFrame(self, bg_color="#FFF", height=800)
+        self.content_frame.pack(side="bottom", fill="both", expand=True)
+
     def toggle_menu(self):
         if self.menu_visible:
-            self.menu_frame.place(x=-200, y=self.app_title.winfo_height())  # Place menu offscreen
+            self.menu_frame.place(x=-900, y=self.app_title.winfo_height())  # Place menu offscreen
             self.menu_visible = False
         else:
             self.menu_frame.place(x=0, y=self.app_title.winfo_height())  # Place menu below app title
             self.menu_visible = True
+            self.menu_frame.lift()  # Bring menu to front
 
 
 # Create the main CustomTkinter window and add the NavigationBar
