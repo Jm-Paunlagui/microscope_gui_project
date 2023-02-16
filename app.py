0bb5ca9dc037d66cc96c58ca9ff6548f8790c230
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2023-02-16 23:45:11 +0800

    cleaner look but will add a top bar

--- app.py
+++ app.py
@@ -39,8 +39,7 @@ class App(customtkinter.CTk):
         self.geometry(f"{1280}x{832}")
 
         # @description: min and max size of window
-        self.minsize(1280, 832)
-        self.maxsize(1280, 832)
+        # self.minsize(1280, 832)
 
         # @description: Window background color gray
         self.configure(fg_color="#d1d5db")
@@ -54,68 +53,62 @@ class App(customtkinter.CTk):
         self.sidebar_frame.grid(row=0, column=0, rowspan=3, sticky="nsew")
 
         # @description: Rows Config for sidebar_frame
-        self.sidebar_frame.grid_rowconfigure(10, weight=1)
+        self.sidebar_frame.grid_rowconfigure(15, weight=1)
 
         # @description: Sidebar OptionMenu
-        self.label_options = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter",
+        self.label_options = customtkinter.CTkLabel(self.sidebar_frame, text="Microscope Options",
                                                     font=customtkinter.CTkFont(size=20, weight="bold"))
         self.label_options.grid(row=0, column=0, padx=20, pady=(20, 10))
         self.sidebar_option_1 = customtkinter.CTkOptionMenu(
             self.sidebar_frame, values=["Option 1", "Option 2", "Option 3"], command=test_event)
-        self.sidebar_option_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_option_1.grid(row=1, column=0, padx=0, pady=10)
         self.sidebar_option_2 = customtkinter.CTkOptionMenu(
             self.sidebar_frame, values=["Option 1", "Option 2", "Option 3"], command=test_event)
-        self.sidebar_option_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_option_2.grid(row=2, column=0, padx=0, pady=10)
         self.sidebar_option_3 = customtkinter.CTkOptionMenu(
             self.sidebar_frame, values=["Option 1", "Option 2", "Option 3"], command=test_event)
-        self.sidebar_option_3.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_option_3.grid(row=3, column=0, padx=0, pady=10)
         self.sidebar_option_4 = customtkinter.CTkOptionMenu(
             self.sidebar_frame, values=["Option 1", "Option 2", "Option 3"], command=test_event)
-        self.sidebar_option_4.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_option_4.grid(row=4, column=0, padx=0, pady=10)
 
-        # @description: 2 SegmentedButton for appearance mode
-        self.label_options_1 = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter",
-                                                      font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.label_options_1.grid(row=5, column=0, padx=20, pady=10)
         self.sidebar_segmented_button_1 = customtkinter.CTkSegmentedButton(self.sidebar_frame,
                                                                            values=["Button 1", "Button 2"],
                                                                            command=test_event)
-        self.sidebar_segmented_button_1.grid(row=6, column=0, padx=20, pady=10)
+        self.sidebar_segmented_button_1.grid(row=5, column=0, padx=0, pady=10)
         self.sidebar_segmented_button_1.set("Button 1")
-
-        # @description: SegmentedButton
-        self.label_options_2 = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter",
-                                                      font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.label_options_2.grid(row=8, column=0, padx=20, pady=10)
 
         # @description: Combobox with values
         self.sidebar_combobox_1 = customtkinter.CTkOptionMenu(
             self.sidebar_frame, values=["Option 1", "Option 2", "Option 3"], command=test_event)
-        self.sidebar_combobox_1.grid(row=9, column=0, padx=20, pady=10)
+        self.sidebar_combobox_1.grid(row=6, column=0, padx=0, pady=10)
+
+        # @description: Camera buttons
+        self.label_options_3 = customtkinter.CTkLabel(self.sidebar_frame, text="Camera Options",
+                                                      font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.label_options_3.grid(row=7, column=0, padx=0, pady=10)
+        self.camera_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Button 1",
+                                                       command=sidebar_button_event)
+        self.camera_button_1.grid(row=8, column=0, padx=0, pady=10, sticky="s")
+        self.camera_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Button 2",
+                                                       command=sidebar_button_event)
+        self.camera_button_2.grid(row=9, column=0, padx=0, pady=10, sticky="s")
+        self.camera_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Button 3",
+                                                       command=sidebar_button_event)
+        self.camera_button_3.grid(row=10, column=0, padx=0, pady=10, sticky="s")
+        self.camera_button_4 = customtkinter.CTkButton(self.sidebar_frame, text="Button 4",
+                                                       command=sidebar_button_event)
+        self.camera_button_4.grid(row=11, column=0, padx=0, pady=10, sticky="s")
 
         # @description: Main Frame for the camera with 4 buttons
         self.main_frame = customtkinter.CTkFrame(self, corner_radius=10, fg_color="#f8fafc")
         self.main_frame.grid(row=0, column=1, rowspan=2, sticky="nsew", padx=10, pady=10)
 
         # @description: Canvas for the camera image
-        self.camera_canvas = customtkinter.CTkCanvas(self.main_frame, width=1050, height=730, relief="flat")
-        self.camera_canvas.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
-
+        self.camera_canvas = customtkinter.CTkCanvas(self.main_frame, width=self.winfo_y(),
+                                                     height=self.winfo_x(), relief="flat")
+        self.camera_canvas.grid(row=0, column=0, columnspan=4, padx=2, pady=2, sticky="nsew")
         # @description: Camera VideoCapture
-
-        # @description: 4 Buttons for the camera in the main frame (bottom center and aligned to the bottom)
-        self.camera_button_1 = customtkinter.CTkButton(self.main_frame, text="Button 1",
-                                                       command=sidebar_button_event)
-        self.camera_button_1.grid(row=1, column=0, padx=10, pady=10, sticky="s")
-        self.camera_button_2 = customtkinter.CTkButton(self.main_frame, text="Button 2",
-                                                       command=sidebar_button_event)
-        self.camera_button_2.grid(row=1, column=1, padx=10, pady=10, sticky="s")
-        self.camera_button_3 = customtkinter.CTkButton(self.main_frame, text="Button 3",
-                                                       command=sidebar_button_event)
-        self.camera_button_3.grid(row=1, column=2, padx=10, pady=10, sticky="s")
-        self.camera_button_4 = customtkinter.CTkButton(self.main_frame, text="Button 4",
-                                                       command=sidebar_button_event)
-        self.camera_button_4.grid(row=1, column=3, padx=10, pady=10, sticky="s")
 
 
 if __name__ == "__main__":
